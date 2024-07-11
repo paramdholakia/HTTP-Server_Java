16d55ee222f300502ef57f2e27948ce4ea88ff9c
ParamDholakiya <paramdholakia@gmail.com>
2024-07-11 20:49:43 +0530

    first commit - set up of file structure

---
+++ basichttpserver/src/main/java/com/paramdholakia/basichttpserver/HttpServer.java
@@ -0,0 +1,11 @@
+package com.paramdholakia.basichttpserver;
+/*
+ * 
+ * Driver Class for HttpServer
+ * */
+
+public class HttpServer {
+	public static void main(String[] args) {
+		System.out.println("Server Started...");
+	}
+}
